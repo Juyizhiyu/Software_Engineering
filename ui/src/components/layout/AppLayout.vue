9673c664248d26c8abd22ecc24e4cd9353bca9ff
HarryHelloo <hiharry-mdq@foxmail.com>
2026-06-04 11:50:38 +0800

    refactor(ui/layout): 移除全局布局中默认的header插槽容器

--- ui/src/components/layout/AppLayout.vue
+++ ui/src/components/layout/AppLayout.vue
@@ -9,14 +9,6 @@ const collapsed = ref(false)
   <el-container class="app-layout">
     <AppSidebar v-model:collapsed="collapsed" />
     <el-container>
-      <el-header class="app-header">
-        <div class="app-header__left">
-          <slot name="header" />
-        </div>
-        <div class="app-header__right">
-          <slot name="header-extra" />
-        </div>
-      </el-header>
       <el-main class="app-main">
         <router-view v-slot="{ Component }">
           <transition name="fade" mode="out-in">
@@ -31,23 +23,6 @@ const collapsed = ref(false)
 <style scoped lang="scss">
 .app-layout {
   height: 100vh;
-}
-
-.app-header {
-  @include flex-between;
-  padding: 0 $spacing-lg;
-  background: var(--app-bg-card);
-  border-bottom: 1px solid var(--el-border-color-lighter);
-
-  &__left {
-    display: flex;
-    align-items: center;
-  }
-
-  &__right {
-    display: flex;
-    align-items: center;
-  }
 }
 
 .app-main {
